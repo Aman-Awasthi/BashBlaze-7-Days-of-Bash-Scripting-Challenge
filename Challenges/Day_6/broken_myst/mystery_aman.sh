d0f9cbcdaa5c957794b6890c54eb1a072375adc1
Ubuntu <ubuntu@ip-172-31-86-61.ec2.internal>
2023-08-06 17:19:54 +0000

    Day 6

--- Challenges/Day_6/broken_myst/mystery_aman.sh
+++ Challenges/Day_6/broken_myst/mystery_aman.sh
@@ -12,24 +12,24 @@ mysterious_function() {
     local input_file="$1"
     local output_file="$2"
     
-    # 
+    # Reverse the characters in the input file using the 'tr' command
     tr 'A-Za-z' 'N-ZA-Mn-za-m' < "$input_file" > "$output_file"
 
-    # 
+    # Reverse the characters in the output file again using the 'rev' command and store it in a temporary file called 'reversed_temp.txt'
     rev "$output_file" > "reversed_temp.txt"
 
-    # 
+    # Generate a random number between 1 and 10 (inclusive) using 'RANDOM' and store it in the variable 'random_number'
     random_number=$(( ( RANDOM % 10 ) + 1 ))
 
-    # Mystery loop: 
+    # Mystery loop: Repeat the next set of steps 'random_number' times
     for (( i=0; i<$random_number; i++ )); do
-        # 
+        # Reverse the characters in the 'reversed_temp.txt' (temporary) file using 'rev' and store it in another temporary file 'temp_rev.txt'
         rev "reversed_temp.txt" > "temp_rev.txt"
 
-        # 
+        # Apply the same character transformation and reverse the characters in the input file using the 'tr' command on the 'temp_rev.txt' and store the result in 'temp_enc.txt'
         tr 'A-Za-z' 'N-ZA-Mn-za-m' < "temp_rev.txt" > "temp_enc.txt"
 
-        # 
+        #  Move the 'temp_enc.txt' to 'reversed_temp.txt' for the next iteration
         mv "temp_enc.txt" "reversed_temp.txt"
     done
 
